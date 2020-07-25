29486d423052b9a87f611554b5edfb3b27b9fa6a
Naasir Salafee <naasirbush@icloud.com>
2020-07-24 23:10:30 -0700

    Task 2 : Workshop

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import { connect } from "react-redux";
 import { actions } from "react-redux-form";
 import {
     postComment,
+    postFeedback,
     fetchCampsites,
     fetchComments,
     fetchPromotions,
@@ -30,6 +31,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = {
     postComment: (campsiteId, rating, author, text) =>
         postComment(campsiteId, rating, author, text),
+    postFeedback: (feedback) => postFeedback(feedback),
     fetchCampsites: () => fetchCampsites(),
     resetFeedbackForm: () => actions.reset("feedbackForm"),
     fetchComments: () => fetchComments(),
@@ -114,7 +116,10 @@ class Main extends Component {
                                 exact
                                 path="/contactus"
                                 render={() => (
-                                    <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+                                    <Contact
+                                        postFeedback={this.props.postFeedback}
+                                        resetFeedbackForm={this.props.resetFeedbackForm}
+                                    />
                                 )}
                             />
                             <Route
